2708c0ae21d1e03fa6054b0b411314358f6e379d
uimkatali <b8bf6b5@gmail.com>
2024-02-01 11:43:22 +0200

    Modify mocks

--- src/MOCKDATA.ts
+++ src/MOCKDATA.ts
@@ -1,8 +1,8 @@
 export const MOCK = [
-  { _id: '65261eeb66kkkkkkk44d7ba5', name: 'Catalin Miu', age: '255', role: 'admin' },
-  { _id: '65261eeb66cc7efb644d7ba5', name: 'Catalin Miu', age: '255', role: 'admin' },
+  { _id: '65261eeb66kkkkkkk44d7ba5', name: 'Catalin', age: '255', role: 'admin' },
+  { _id: '65261eeb66cc7efb644d7ba5', name: 'Catalin', age: '255', role: 'admin' },
   { _id: '65261eeb69cc7afb64cd7ba5', name: 'Nicusor', age: '251', role: 'user' },
   { _id: '65261eeb66cc7efb64cd7ac5', name: 'Ayia', age: '44', role: 'admin' },
-  { _id: '65261ffb66ff7efb64cd7ac5', name: 'Ana Georgescu', age: '27', role: 'admin' },
+  { _id: '65261ffb66ff7efb64cd7ac5', name: 'Ana', age: '27', role: 'admin' },
   { _id: '65261eeb66dd7efb644ddba5', name: 'C C', age: '19', role: 'user' },
 ]
